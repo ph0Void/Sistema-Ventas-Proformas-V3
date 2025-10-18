f1f1294e11206636e2f3e1d14e8f9576501f7b0a
ph0Void <155586718+ph0Void@users.noreply.github.com>
2025-10-18 11:20:18 -0500

    Remove Javadoc comments from ICashOrderService
    Removed Javadoc comments from ICashOrderService interface.

--- src/main/java/com/TiendaVirtual/proyecto/service/common/ICashOrderService.java
+++ src/main/java/com/TiendaVirtual/proyecto/service/common/ICashOrderService.java
@@ -2,13 +2,6 @@ package com.TiendaVirtual.proyecto.service.common;
 
 import java.util.List;
 
-/**
- * Interfaz genérica para servicios de gestión de órdenes de caja, como ventas y proformas.
- *
- * @param <E>  entidad de la SALE | PROFORMA
- * @param <D>  entidad de la SALE | PROFORMA  respuesta
- * @param <I>  id de la entidad y el id del vendedor
- */
 public interface ICashOrderService <E, D , I > {
 
     D createOrder(E order, I idSeller);
